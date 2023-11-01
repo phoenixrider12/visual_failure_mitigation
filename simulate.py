0a23cb774234b9ec491f72fccb71ac2fb2f2c416
phoenixrider12 <aryaman.gupta.ece20@itbhu.ac.in>
2023-11-01 21:52:05 +0530

    changing simulator utils loading

--- simulate.py
+++ simulate.py
@@ -1,5 +1,6 @@
 import os
 import importlib
+from sim_helper.simulator_utils_KMWH import *      # Change according to set airport
 
 ########### Set PARAMS here ############
 TIME_OF_DAY = 17.0 # military time
@@ -8,14 +9,14 @@ RUNWAY = 'KMWH'
 START_POS = [-6, 110.0, -17.0] # [CTE, DTP, HE]
 USE_FALLBACK = True
 
-sim_helper_dir = "sim_helper"
-sim_helper_filename = 'sim_helper.simulator_utils_{}.py'.format(RUNWAY)
-sim_helper_file_path = os.path.join(sim_helper_dir, sim_helper_filename)
+# sim_helper_dir = "sim_helper"
+# sim_helper_filename = 'sim_helper.simulator_utils_{}.py'.format(RUNWAY)
+# sim_helper_file_path = os.path.join(sim_helper_dir, sim_helper_filename)
 
-if os.path.exists(sim_helper_file_path):
-    spec = importlib.util.spec_from_file_location(sim_helper_filename, sim_helper_file_path)
-    sim_utils = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(sim_utils)
+# if os.path.exists(sim_helper_file_path):
+#     spec = importlib.util.spec_from_file_location(sim_helper_filename, sim_helper_file_path)
+#     sim_utils = importlib.util.module_from_spec(spec)
+#     spec.loader.exec_module(sim_utils)
 
 foldername = 'simulation'
 save_traj = 0
@@ -44,7 +45,7 @@ start_params = {'foldername': foldername,
                 }
 
 
-client, network, classifier = sim_utils.get_simulator_and_model(start_params, MODEL_WEIGHTS)
+client, network, classifier = get_simulator_and_model(start_params, MODEL_WEIGHTS)
 
 # Start the simulation
 xs, ys, thetas, pred_xs, pred_thetas, control, orig_images, downsampled_images, result, episode_steps, predicted_images, predicted_downsampled_images, xs_record, he_record, ys_record, velocities = rollout(client, network, 
@@ -56,7 +57,7 @@ xs, ys, thetas, pred_xs, pred_thetas, control, orig_images, downsampled_images, 
                                                                                                                 )
 
 if save_traj:
-    sim_utils.save_metrics(xs, ys, thetas, pred_xs, pred_thetas, control, orig_images, downsampled_images, result, episode_steps, start_params, predicted_images, predicted_downsampled_images)
+    save_metrics(xs, ys, thetas, pred_xs, pred_thetas, control, orig_images, downsampled_images, result, episode_steps, start_params, predicted_images, predicted_downsampled_images)
 
 if trajectory_plot:
-    sim_utils.plot_trajectory(xs_record, ys_record, USE_FALLBACK)
+    plot_trajectory(xs_record, ys_record, USE_FALLBACK)
